203856dd7ca336d5aab0b7b2e1b79ffa4da4d0ff
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-03-22 10:11:09 -0400

    added navbar.handlebars partial, added dashboard.handlebars, added login.handlebars, renames script.js to login.js, added js folder to public dir, added userRoutes.js to api routes dir, added login post route and logout post route

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!user) {
+      res.status(400).json({ message: "No user account found!" });
+      return;
+    }
+
+    const validPassword = user.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "No user account found!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.userId = user.user_id;
+      req.session.email = user.email;
+      req.session.loggedIn = true;
+
+      res.json({ user, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
