f593293eb25448dc468c557ec754faf55c20b2bf
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-03-26 17:26:31 -0400

    finally fixed the model problems by requiring the models in the routes from the index file where relationships are created. Thanks Carl

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const User = require("../../Models/User");
 const Post = require("../../Models/Post");
 const withAuth = require("../../utils/auth");
-
+const Comment = require("../../Models/Comment");
 // This is the api/user route
 
 router.get("/", async (req, res) => {
@@ -113,8 +113,8 @@ router.get("/comment", async (req, res) => {
       //   post_id: req.body.post_id,
       // },
     });
-    const comments = commentData.map((c) => c.get({ plain: true }));
-    res.status(200).json(comments);
+    // const comments = commentData.map((c) => c.get({ plain: true }));
+    res.status(200).json(commentData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -124,8 +124,23 @@ router.post("/comment", async (req, res) => {
   try {
     const response = await Comment.create({
       post_id: req.body.post_id,
-      user_id: req.session.user_id,
+      // user_id: req.session.user_id,
+      user_id: req.body.user_id,
       comment_text: req.body.comment_text,
+    });
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/test/:id", async (req, res) => {
+  try {
+    const response = await User.findOne({
+      include: Post,
+      where: {
+        user_id: req.params.id,
+      },
     });
     res.status(200).json(response);
   } catch (err) {
